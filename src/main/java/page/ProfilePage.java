ab1a10f523df3a357915d37fd3a7cfd49fe40a40
wangzibin <zhibin_wang219@163.com>
2019-01-16 15:39:12 +0800

    change find login method

--- src/main/java/page/ProfilePage.java
+++ src/main/java/page/ProfilePage.java
@@ -1,19 +1,12 @@
 package page;
 
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 
 public class ProfilePage extends BasePage {
 
     public LoginPage gotoLoginPage(){
 
-            while (true){
-                WebElement element = find(text("点击登录"));
-                if(element.isDisplayed()) {
-                    element.click();
-                    break;
-                }
-
-            }
+        find(By.xpath("//*[@text='点击登录']")).click();
 
         return new LoginPage();
     }
